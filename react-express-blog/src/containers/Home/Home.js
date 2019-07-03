252f2c0594c56377973c737599bddca89c54136f
GFLEE <leegradylll@gmail.com>
2019-07-04 01:08:13 +0800

    homeCompnent

---
+++ react-express-blog/src/containers/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import style from './style.css';
+import Header from '../../components/Header/Header';
+
+
+class Home extends Component {
+    render() {
+        return (
+            <h1>Blog</h1>
+        )
+    }
+};
+
+export default Home;
